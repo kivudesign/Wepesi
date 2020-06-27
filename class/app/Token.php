b15eb529981cf69e571c5e47dcd388609c2ec0a2
bim-g <ibmussafb@gmail.com>
2020-06-27 11:22:22 +0200

    implement session and token class for input validation

---
+++ class/app/Token.php
@@ -0,0 +1,16 @@
+<?php
+    
+    class Token{
+        static function generate(){
+            return Session::put("session/token_name",md5(uniqid()));
+        }
+        static function check($token){
+            $tokenName=config::get("session/token_name");
+            if(Session::exists($tokenName && $token===Session::get($tokenName))){
+                Session::delete($tokenName);
+                return true;
+            }
+            return false;
+        }
+    }
+?>
