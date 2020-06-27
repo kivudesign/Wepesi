9cb3f287d15ba0c2406e2e1efb40290a2fdde77e
bim-g <ibmussafb@gmail.com>
2020-06-27 11:23:21 +0200

    implement redirect class for page rediction

---
+++ class/app/Redirect.php
@@ -0,0 +1,20 @@
+<?php
+    class Redirect{
+        static function to($location=null){
+            if($location){
+                if(is_numeric($location)){
+                    $location="error/";
+                    switch($location){
+                        case 404: $location.="404";
+                        break;
+                    }
+                }else if(strpos($location, ".php")){
+                    header('Location:'.$location);
+                }else{
+                    header('Location:' . $location);
+                }
+                exit();
+            }
+        }
+    }
+?>
