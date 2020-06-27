3013d7eef9c0054a2dc37e3434c59bb92e8f6387
bim-g <ibmussafb@gmail.com>
2020-06-27 11:14:29 +0200

    implement general controller class

---
+++ class/app/View.php
@@ -0,0 +1,22 @@
+<?php
+    class View{
+        private $data=[];
+        private $render=false;
+
+        function __construct($file)
+        {
+            if (is_file(ROOT . "views/" . $file . ".php")) { 
+                $this->render=ROOT . "views/" . $file . ".php"; 
+            }
+        }
+
+        function assign($variable,$value){
+            $this->data[$variable]=$value;
+        }
+        function __destruct()
+        {
+            extract($this->data);
+            include($this->render);
+        }
+    }
+?>
