338ad1bbeb1ca0f651b755f812e8b039a3c211bd
Vainqueur Bihame <34940230+WinnersProx@users.noreply.github.com>
2020-07-01 12:36:45 +0200

    Update Hash.php
    Could you use password_hash ( string $password , mixed $algo [, array $options ] ) : string instead of using hash?

--- class/app/Hash.php
+++ class/app/Hash.php
@@ -1,6 +1,8 @@
 <?php    
     class Hash{
         static function make($string,$salt=""){
+            // could use password_hash instead of hash only so that you may use the BCRYPT as the hashing algorythm
+            // check https://www.php.net/manual/en/function.password-hash.php
             return hash('sha256',$string.$salt);
         }
         static function salt($length){
