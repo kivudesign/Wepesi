366ff588a0895de7a4f234f856d1a49d08c358c0
bim-g <ibmussafb@gmail.com>
2020-08-21 12:43:38 +0200

    fix token validation error

--- class/app/Token.php
+++ class/app/Token.php
@@ -6,7 +6,7 @@
         }
         static function check($token){
             $tokenName=Config::get("session/token_name");
-            if(Session::exists($tokenName && $token===Session::get($tokenName))){
+            if(Session::exists($tokenName) && $token===Session::get($tokenName)){
                 Session::delete($tokenName);
                 return true;
             }
