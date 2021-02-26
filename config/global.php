bea09f5cdf23f7a3aeca77735d227b1c691d9371
bim-g <ibmussafb@gmail.com>
2021-02-26 11:16:53 +0200

    fix validation language

--- config/global.php
+++ config/global.php
@@ -8,6 +8,13 @@ define("DATABASE", $db_conf->database);
 define("USER", $db_conf->user);
 define("PASSWORD", $db_conf->password);
 
+//  default en: english, for french: fr
+define("LANG", "en");
+//web root configaration
+include("./lang/" . LANG . ".php");
+define("LANG_VALIDATION", $validation);
+define("LANG_BOX_MESSAGE", $boxMessage);
+
 //web root configaration
     define('WEB_ROOT', str_replace('index.php', '', $_SERVER['SCRIPT_NAME']));
     define('ROOT', str_replace('index.php', '', $_SERVER['SCRIPT_FILENAME']));
