61031ada2ab454e17605ea164b48b9bafbccbe7d
bim-g <ibmussafb@gmail.com>
2021-02-26 11:07:21 +0200

    add check file extension

--- class/app/Controller.php
+++ class/app/Controller.php
@@ -1,14 +1,16 @@
 <?php
     class Controller{
         
-        static function useModel($file)        {
-            if (is_file(ROOT . 'corp/' . $file . ".php")) {
-                require_once(ROOT . 'corp/' . $file . '.php');
+        static function useModel($fileName)        {
+            $file= check_file_extention($fileName);
+            if (is_file(ROOT . 'corp/' . $file )) {
+                require_once(ROOT . 'corp/' . $file);
             }
         }
-        static function useController($file)        {
-            if (is_file(ROOT . 'controller/' . $file . ".php")) {
-                require_once(ROOT . 'controller/' . $file . '.php');
+        static function useController($fileName)        {
+            $file= check_file_extention($fileName);
+            if (is_file(ROOT . 'controller/' . $file)) {
+                require_once(ROOT . 'controller/' . $file );
             }
         }       
     }
