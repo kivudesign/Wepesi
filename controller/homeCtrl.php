75571213eb8944f12b4d276f7ff236f45c47305a
bim-g <ibmussafb@gmail.com>
2021-11-14 03:24:01 +0200

    [FIX] remove unused function module

--- controller/homeCtrl.php
+++ controller/homeCtrl.php
@@ -1,12 +1,9 @@
 <?php
-    use Demo\View;
     class homeCtrl{
         function __construct()
         {
-            $this->h= new Home();
         }
         function home(){
-            $v=new View('index');
-            $v->assign("result",$this->h->welcom());
-        }        
+            new View('index');
+        }
     }
