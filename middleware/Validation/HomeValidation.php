4899b361ef9fd1b35200b92dd7f5063f7b103923
bim-g <ibmussafb@gmail.com>
2022-06-15 19:39:43 +0200

    [UPD] HomeValidation:add validation from submit input.

--- middleware/Validation/HomeValidation.php
+++ middleware/Validation/HomeValidation.php
@@ -1,14 +1,29 @@
 <?php
 namespace wepesi\Validation;
 
-use Wepesi\Core\Input;
-use Wepesi\Core\Redirect;
-use Wepesi\Core\Session;
 use Wepesi\Core\Validation\Validate;
 
 class HomeValidation
 {
     function changeLang(){
-        // implement your validation middleware for language params
+        $valid = new Validate();
+        $schema = [
+            "token" => $valid->string("token")
+                ->min(1)
+                ->max(2)
+                ->required()
+                ->check(),
+            "lang" => $valid->string("lang")
+                ->min(1)
+                ->max(2)
+                ->required()
+                ->check()
+        ];
+
+        $valid->check($_POST,$schema);
+        if(!$valid->passed()){
+            var_dump($valid->errors());
+            die();
+        }
     }
 }
---
+++ middleware/Validation/index.php
@@ -0,0 +1,5 @@
+<?php
+if ( $_SERVER['REQUEST_METHOD']=='GET' && realpath(__FILE__) == realpath( $_SERVER['SCRIPT_FILENAME'] ) ) {
+    header( 'HTTP/1.0 403 Forbidden', TRUE, 403 );
+    die( header( 'location:'.str_replace('index.php', '', $_SERVER['SCRIPT_FILENAME']) ) );
+}
