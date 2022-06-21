50bc3be7fd53351d26f9b58ed158dcfb07e2ef6c
bim-g <ibmussafb@gmail.com>
2022-06-22 00:29:43 +0200

    [ENH] Escape moodule: review and rewrite all procedure.

--- src/Core/Escape.php
+++ src/Core/Escape.php
@@ -3,15 +3,28 @@
 namespace Wepesi\Core;
 
 class Escape{
-
-    static function encode(string $input){
+    static function encode(string $input)
+    {
         $text = $input;
         if ($input != strip_tags($input)) {
             $text = htmlentities($input, ENT_QUOTES, 'UTF-8');
         }
         return $text;
     }
-    static function decode(string $input){
+
+    static function decode(string $input)
+    {
         return html_entity_decode($input, ENT_QUOTES, 'UTF-8');
+    }
+
+    static function randomString(int $length = 8)
+    {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return $randomString;
     }
 }
