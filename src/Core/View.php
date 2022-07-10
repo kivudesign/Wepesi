005e55065333eca5acfcb4a59e7dcd00c84d5971
bim-g <ibmussafb@gmail.com>
2022-07-10 18:35:44 +0200

    [UPD] make view to be more dynamic with paths.

--- src/Core/View.php
+++ src/Core/View.php
@@ -2,45 +2,49 @@
 
 namespace Wepesi\Core;
 
-    class View{
-        private array $data=[];
-        const ERROR=ROOT."views/404.php";
-        private string $render;
-        private $folder_name;
+class View
+{
+    private array $data = [];
+    const ERROR = ROOT . 'views/404.php';
+    private string $render;
+    private string $folder_name;
 
-        function __construct($folder="/")
-        {
-            $this->render=self::ERROR;
-            $this->folder_name=$folder;
-        }
+    function __construct(string $folder_name = '/')
+    {
+        $this->render = self::ERROR;
+        $this->folder_name = Escape::addSlaches($folder_name);
+    }
 
-        /**
-         * call this method to display file content
-         * @param string $file_name
-         */
-        function display(string $file_name){
-            $file = checkFileExtension($file_name);
-            $file_source = substr($file, 0) == '/' ? $this->folder_name . $file : $this->folder_name . '/' . $file;
-            if (is_file(ROOT . 'views' . $file_source)) {
-                $this->render = ROOT . 'views' . $file_source;
-            }
-        }
-
-        /**
-         * assign variables data to be displayed on file_page
-         * @param string $variable
-         * @param $value
-         */
-        function assign(string $variable,$value){
-            $this->data[$variable]=$value;
-        }
-        function __destruct()
-        {
-            extract($this->data);
-            if (is_file($this->render)) {
-                include($this->render);
-            }else{
-                print_r(["exception"=>"file path is not correct.?"]);
-            }
+    /**
+     * call this method to display file content
+     * @param string $file_name
+     */
+    function display(string $file_name)
+    {
+        $file = Escape::checkFileExtension($file_name);
+        $file_source = $this->folder_name . Escape::addSlaches($file);
+        if (is_file(ROOT . 'views' . $file_source)) {
+            $this->render = ROOT . 'views' . $file_source;
         }
     }
+
+    /**
+     * assign variables data to be displayed on file_page
+     * @param string $variable
+     * @param $value
+     */
+    function assign(string $variable, $value)
+    {
+        $this->data[$variable] = $value;
+    }
+
+    function __destruct()
+    {
+        extract($this->data);
+        if (is_file($this->render)) {
+            include($this->render);
+        } else {
+            print_r(['exception' => 'file path is not correct.?']);
+        }
+    }
+}
