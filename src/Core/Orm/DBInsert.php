5ad8fb86a11aef22d8fa979b34fec244bdb39a75
bim-g <ibmussafb@gmail.com>
2023-07-27 22:33:20 +0200

    [ENH] Refactoring ORM module.

--- src/Core/Orm/DBInsert.php
+++ src/Core/Orm/DBInsert.php
@@ -2,108 +2,97 @@
 
 namespace Wepesi\Core\Orm;
 
-class DBInsert
+use Wepesi\Core\Orm\Provider\DbProvider;
+
+/**
+ *
+ */
+class DBInsert extends DbProvider
 {
-    private $table, $_pdo;
-    private $_fields;
-    private  $_error,
-        $_results = false,
-        $_count = 0,
-        $_lastid;
-    function __construct(\PDO $pdo, string $table)
+    /**
+     * @var string
+     */
+    private string $table;
+    /**
+     * @var array
+     */
+    private array $_fields;
+
+
+    /**
+     * @param \PDO $pdo
+     * @param string $table
+     */
+    public function __construct(\PDO $pdo, string $table)
     {
         $this->table = $table;
-        $this->_pdo = $pdo;
+        $this->pdo = $pdo;
+        $this->_fields = [];
+        $this->_results = [];
+        $this->lastID = 0;
+        $this->_error = '';
     }
 
-    //
-    function field(array $fields)
+    /**
+     * profide field to be saved
+     * @param array $fields
+     * @return $this
+     */
+    public function field(array $fields): DBInsert
     {
-        if (count($fields) && !$this->_fields ) {
-            $params = $fields;
-            $x = 1;
+        if (count($fields) && !$this->_fields) {
+            $field_key_position = 0;
             $keys = array_keys($fields);
             $values = null;
-            $_trim_key=[];
-            foreach ($fields as $f) {
-                $values .= "? ";
-                if ($x < count($fields)) {
+            $trim_key = [];
+            foreach ($fields as $field) {
+                $values .= '? ';
+                if (count($fields) > ($field_key_position + 1)) {
                     $values .= ', ';
                 }
                 //remove white space around the field name
-                $_trim_key[]=trim($keys[($x-1)]);
-                $x++;
+                $trim_key[] = trim($keys[$field_key_position]);
+                $field_key_position++;
             }
-            $all_keys=$_trim_key;
-            $implode_keys= "`" . implode('`,`', $all_keys) . "`";
+
+            $implode_keys = '`' . implode('`,`', $trim_key) . '`';
 
             $this->_fields = [
-                "keys" => $implode_keys,
-                "values" => $values,
-                "params" => $params
+                'keys' => $implode_keys,
+                'values' => $values,
+                'params' => $fields
             ];
         }
         return $this;
     }
 
     /**
-     * @param string $sql
-     * @param array $params
-     * @return void
-     * this module is use to execute sql request
-     */
-    private function query(string $sql, array $params = [])
-    {
-        $q = new DBQuery($this->_pdo, $sql, $params);
-        $this->_results = $q->result();
-        $this->_count = $q->rowCount();
-        $this->_error = $q->getError();
-        $this->_lastid = $q->lastId();
-    }
-
-    /**
-     * @return bool
-     * use this module to create new record
-     */
-    private function insert()
-    {
-        $fields = $this->_fields['keys'];
-        $values =  $this->_fields['values'];
-        $params = $this->_fields['params'];
-        $sql = "INSERT INTO $this->table ($fields) VALUES ($values)";
-        return $this->query($sql, $params);
-    }
-
-    /**
-     * @return bool
+     * @return array
      * return result after a request select
      */
-    function result()
+    public function result(): array
     {
         $this->insert();
         return $this->_results;
     }
-    // return an error status when an error occure while doing an querry
-    function error()
+
+    /**
+     *
+     */
+    private function insert()
     {
-        return $this->_error;
+        $fields = $this->_fields['keys'];
+        $values = $this->_fields['values'];
+        $params = $this->_fields['params'];
+        $sql = "INSERT INTO $this->table ($fields) VALUES ($values)";
+        $this->query($sql, $params);
     }
 
     /**
      * @return int
-     * return counted rows of a select query
      */
-    function count()
+    public function lastId(): int
     {
-        return $this->_count;
-    }
-
-    /**
-     * @return mixed
-     * access the last id record after creating a new record
-     */
-    function lastId()
-    {
-        return $this->_lastid;
+        return $this->lastID;
     }
 }
---
+++ src/Core/Orm/Provider/Contract/DbContract.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Wepesi\Core\Orm\Provider\Contract;
+
+interface DbContract
+{
+    function error() : string;
+    function result() : array;
+}
