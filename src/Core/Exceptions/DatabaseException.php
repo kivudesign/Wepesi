f8efeea448c8406c5e2a800782df472678eb1d2f
bim-g <ibmussafb@gmail.com>
2024-09-23 23:04:59 +0200

    [ENH] Add database exception class

---
+++ src/Core/Exceptions/DatabaseException.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * Copyright (c) 2024. Wepesi Dev Framework
+ */
+
+namespace Wepesi\Core\Exceptions;
+/**
+ * @package Wepesi\Core\Exceptions
+ */
+final class DatabaseException extends WepesiException
+{
+
+}
