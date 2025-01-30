8dfa95a357949ae433c14545568b3a3565c55b36
bim-g <ibmussafb@gmail.com>
2025-01-30 21:33:34 +0200

    [UDP] add Entity Contracts.

--- src/Core/Database/Providers/Contracts/EntityContracts.php
+++ src/Core/Database/Providers/Contracts/EntityContracts.php
@@ -2,9 +2,19 @@
 
 namespace Wepesi\Core\Database\Providers\Contracts;
 
+/**
+ * @package Wepesi\Core\Database
+ * @template T
+ */
 interface EntityContracts
 {
+    /**
+     * @return array
+     */
     public function findAll(): array;
 
+    /**
+     * @return array
+     */
     public function findOne(): array;
 }
