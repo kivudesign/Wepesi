97159c39482e685cf2196cc471be0d6206fe4ccc
bim-g <ibmussafb@gmail.com>
2026-05-15 12:30:00 +0200

    [REF] refactoring api registration and add router documentation.

--- src/Core/Routing/Router.php
+++ src/Core/Routing/Router.php
@@ -159,6 +159,30 @@ class  Router implements RouterContract
     }
 
     /**
+     * @param Closure $callable
+     * @return void
+     */
+    public function api(Closure|string $callable): void
+    {
+        if (isset($_SERVER['HTTP_ORIGIN'])) {
+            // Decide if the origin in $_SERVER['HTTP_ORIGIN'] is one
+            // you want to allow, and if so:
+            header('Access-Control-Allow-Origin: *');
+            header('Access-Control-Allow-Credentials: true');
+            header('Access-Control-Max-Age: 86400');    // cache for 1 day
+        }
+        header('Access-Control-Allow-Methods: GET, POST,PUT, PATCH, HEAD, OPTIONS');
+        // Access-Control headers are received during OPTIONS requests
+        if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+            // may also be using PUT, PATCH, HEAD etc.
+            if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+                header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+
+            exit(0);
+        }
+        $this->group('/api', $callable);
+    }
+    /**
      * validate middleware data structure
      * @param $middleware
      * @return callable[]
@@ -257,7 +281,6 @@ class  Router implements RouterContract
                 'status' => '500',
                 'message' => 'Internal Server Error'
             ], 500);
-            return;
         }
     }
 
