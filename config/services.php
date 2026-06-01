b3f8910ec36079e0152e323a361fcba1fba4c0ae
Boss <ibmussafb@gmail.com>
2026-06-01 19:22:44 +0200

    [ENH] Add initial service configuration file to support bindings and singletons

---
+++ config/services.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * Copyright (c) 2026. Wepesi Dev Framework
+ */
+
+return static function (): void {
+    // Application::bind(SomeInterface::class, SomeImplementation::class);
+
+    // Application::singleton(SomeService::class, function () {
+    //     return new SomeService();
+    // });
+};
