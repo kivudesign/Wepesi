b4e6de5e7f18379306aca88837b89da52ef5da4a
Boss <ibmussafb@gmail.com>
2026-07-13 00:46:15 +0200

    [FIX] validate phpstan and phpcs

--- src/Core/Media.php
+++ src/Core/Media.php
@@ -1,145 +1,177 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Wepesi\Core;
 
+use Exception;
+use InvalidArgumentException;
+use RuntimeException;
+
+/**
+ *
+ */
 class Media
 {
-    const maxWidth = 480;
-    const maxHeigth = 400;
-    private string $_target_dir;
+    /**
+     *
+     */
+    private const MAXWIDTH = 480;
+    /**
+     *
+     */
+    private const MAXHEIGHT = 400;
+    /**
+     * @var string
+     */
+    private string $target_dir;
 
-    function __construct(string $target_dir = "public")
+    /**
+     * @param string $target_dir
+     */
+    public function __construct(string $target_dir = "/public")
     {
-        $this->_target_dir = $target_dir;
+        $this->target_dir = $target_dir;
     }
 
-    function uploadImg($file)
+    /**
+     * @param array<string> $file
+     * @return array<string>
+     * @throws Exception
+     */
+    public function uploadImg(array $file): array
     {
-        try {
-            if (!isset($file["name"])) {
-                throw new \Exception("try to access to key `name` witch dos not exist");
-            }
-            $target_file = $this->_target_dir . basename($file["name"]);
-            $filetype = pathinfo($target_file, PATHINFO_EXTENSION);
-            $fileExt = array('jpg', 'jpeg', 'png', 'JPG', 'JPEG', 'PNG');
-            if (!in_array($filetype, $fileExt)) {
-                $this->exception();
-            }
-            if ($res = $this->upload($file, ("photos/" . $filetype), $filetype)) {
-                return $res;
-            }
-        } catch (\Exception $ex) {
-            return ["exception" => $ex->getMessage()];
+        if (!isset($file["name"])) {
+            throw new Exception("try to access to key `name` which does not exist");
         }
-    }
-
-    private function exception(int $src_error = 0): array
-    {
-        $message = "extenstion not supported";
-        switch ($src_error) {
-            case 1:
-                $message = "error thumbnail";
-                break;
-            case 5:
-                $message = "try to access to key `name` witch dos not exist";
-                break;
-            case 20:
-                $message = "unable to upload the file";
-                break;
+        $target_file = $this->target_dir . basename($file['name']);
+        $file_type = pathinfo($target_file, PATHINFO_EXTENSION);
+        $file_extension = array('jpg', 'jpeg', 'png', 'JPG', 'JPEG', 'PNG');
+        if (!in_array($file_type, $file_extension)) {
+            throw new Exception('extension not supported');
         }
-        return ["exception" => $message];
+        if ($res = $this->upload($file, ("photos/" . $file_type), $file_type)) {
+            return $res;
+        }
+        throw new Exception('error uploading image');
     }
 
-    private function upload($file, $formatFile, $filetype)
+    /**
+     * @param array<string> $file
+     * @param string $format_file
+     * @param string $filetype
+     * @return array<string>
+     * @throws Exception
+     */
+    private function upload(array $file, string $format_file, string $filetype): array
     {
-        $Link = $this->_target_dir . '/' . date('Y') . '/' . date('m') . '/';
-        $target_dir = $Link;
+        $target_dir = $this->target_dir . '/' . date('Y') . '/' . date('m') . '/';
 
         if (!file_exists($target_dir)) {
             mkdir($target_dir, 0777, true);
         }
 
-        $filename = $file['name'];
-        $imageRenamed = $this->renameFile($filename);
-        $fileToUpload = (string)$imageRenamed[0];
-        if (!move_uploaded_file($file['tmp_name'], $target_dir . $fileToUpload)) {
-            return $this->exception(10);
+        $file_name = $file['name'];
+        $renamed_file = $this->renameFile($file_name);
+        if (! move_uploaded_file($file['tmp_name'], $target_dir . $renamed_file)) {
+            throw new Exception('unable to upload the file');
         }
-        $format = explode("/", $formatFile);
+        $format = explode("/", $format_file);
         if ($format[0] == "photos") {
-            $value = $this->thumbnail($fileToUpload, $target_dir, $Link, $format[1]);
-            if (!$value) {
-                return $this->exception(1);
+            if (! $this->thumbnail($renamed_file, $target_dir, $target_dir, $format[1])) {
+                throw new Exception('error thumbnail');
             }
         }
         return [
-            "name" => $filename,
+            "name" => $file_name,
             "extension" => $filetype,
-            "link" => $Link . $fileToUpload
+            "link" => $target_dir . $renamed_file
         ];
     }
 
-    private function renameFile($img)
+    /**
+     * @param string $img
+     * @return string
+     */
+    private function renameFile(string $img): string
     {
-        $lastid = TIME();
-        $boom = explode(".", $img);
-        $ext = end($boom);
-        $img = date('y') . Md5($lastid) . date('m') . '.' . $ext;
-        $store = date('y') . Md5($lastid) . date('m');
-        return array($img, $store);
+        $last_id = (string)strtotime("now");
+        $explode = explode(".", $img);
+        $ext = end($explode);
+        $store = date('y') . Md5($last_id) . date('m');
+        return $store . '.' . $ext;
     }
 
-    private function thumbnail($imageToConvert, $source, $dest, $format)
+    /**
+     * @param string $image_to_convert
+     * @param string $source
+     * @param string $dest
+     * @param string $format
+     * @return bool
+     */
+    private function thumbnail(string $image_to_convert, string $source, string $dest, string $format): bool
     {
-        $imageCreated = $source . $imageToConvert;
-        $thumb = null;
-        if ($imageCreated) {
-            list($width, $height) = getimagesize($imageCreated); //$type will return the type of the image
-            $source = $format != "png" ? imagecreatefromjpeg($imageCreated) : imagecreatefrompng($imageCreated);
-
-            if (Media::maxWidth >= $width && Media::maxHeigth >= $height) {
-                $ratio = 1;
-            } elseif ($width > $height) {
-                $ratio = Media::maxWidth / $width;
+        $image_created = $source . $image_to_convert;
+        if ($image_created) {
+            $info = getimagesize($image_created);
+            if ($info !== false) {
+                [$width, $height] = $info;
             } else {
-                $ratio = Media::maxHeigth / $height;
+                throw new RuntimeException('Invalid image file.');
             }
 
-            $thumb_width = round($width * $ratio); //get the smaller value from cal # floor()
-            $thumb_height = round($height * $ratio);
+            $source = $format != "png" ? imagecreatefromjpeg($image_created) : imagecreatefrompng($image_created);
+            if (! $source) {
+                throw new RuntimeException('Failed to create image resource.');
+            }
+            if (Media::MAXWIDTH >= $width && Media::MAXHEIGHT >= $height) {
+                $ratio = 1;
+            } elseif ($width > $height) {
+                $ratio = Media::MAXWIDTH / $width;
+            } else {
+                $ratio = Media::MAXHEIGHT / $height;
+            }
 
+            $thumb_width = (int)round($width * $ratio); //get the smaller value from cal # floor()
+            $thumb_height = (int)round($height * $ratio);
+            if ($thumb_width < 1) {
+                throw new InvalidArgumentException("width should be greater than 0");
+            }
+            if ($thumb_height < 1) {
+                throw new InvalidArgumentException("height should be greater than 0");
+            }
             $thumb = imagecreatetruecolor($thumb_width, $thumb_height);
             imagecopyresampled($thumb, $source, 0, 0, 0, 0, $thumb_width, $thumb_height, $width, $height);
 
-            $path = $dest . $imageToConvert;
-            $image_thumb_path = $path;
+            $path = $dest . $image_to_convert;
             imagejpeg($thumb, $path, 60);
+            imagedestroy($thumb);
+            imagedestroy($source);
             return true;
-        } else {
-            return false;
         }
-        imagedestroy($thumb);
-        imagedestroy($source);
+        return false;
     }
 
-    function uploadSingleFile($file, string $extension_file = null)
+    /**
+     * @param array<string> $file
+     * @param string|null $file_extension
+     * @return array<string>
+     * @throws Exception
+     */
+    public function uploadSingleFile(array $file, ?string $file_extension = null): array
     {
-        try {
-            if (!isset($file["name"])) {
-                throw new \Exception("try to access to key `name` witch dos not exist");
+        if (!isset($file["name"])) {
+            throw new Exception("try to access to key `name` which does not exist");
+        }
+        $target_file = $this->target_dir . basename($file["name"]);
+        $file_type = pathinfo($target_file, PATHINFO_EXTENSION);
+        if ($file_extension) {
+            if ($file_type != $file_extension) {
+                throw new Exception("File type does not match the expected extension");
             }
-            $target_file = $this->_target_dir . basename($file["name"]);
-            $filetype = pathinfo($target_file, PATHINFO_EXTENSION);
-            if ($extension_file) {
-                if ($filetype != $extension_file) {
-                    return $this->exception();
-                }
-                return $this->upload($file, ("$extension_file/" . $filetype), $filetype);
-            } else {
-                return $this->upload($file, ("media/" . $filetype), $filetype);
-            }
-        } catch (\Exception $ex) {
-            return ["exception" => $ex->getMessage()];
+            return $this->upload($file, ("$file_extension/" . $file_type), $file_type);
+        } else {
+            return $this->upload($file, ("media/" . $file_type), $file_type);
         }
     }
 }
