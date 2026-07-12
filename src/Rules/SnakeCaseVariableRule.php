de56fac8071473e89be68f78de9177606c9153c9
Boss <ibmussafb@gmail.com>
2026-07-12 20:50:09 +0200

    [UPD] update validation for class properties

--- src/Rules/SnakeCaseVariableRule.php
+++ src/Rules/SnakeCaseVariableRule.php
@@ -21,36 +21,54 @@ class SnakeCaseVariableRule implements Rule
         if (!is_string($node->name)) {
             return [];
         }
-
-        // Ignore special variables that cannot/should not be renamed.
-        $ignored = [
-            'this',
-            'GLOBALS',
-            '_SERVER',
-            '_GET',
-            '_POST',
-            '_FILES',
-            '_COOKIE',
-            '_SESSION',
-            '_REQUEST',
-            '_ENV',
-            'argc',
-            'argv',
-            'http_response_header',
-        ];
-        $varName = $node->name;
-        if (in_array($varName, $ignored, true)) {
-            return [];
-        }
-
-        if (!preg_match('/^[a-z][a-z0-9]*(_[a-z0-9]+)*$/', $varName)) {
-            return [
-                RuleErrorBuilder::message(
-                    sprintf('Variable $%s must be snake_case.', $varName)
-                )->build()
+        // Local variables
+        if ($node instanceof Node\Expr\Variable) {
+            // Ignore special variables that cannot/should not be renamed.
+            $ignored = [
+                'this',
+                'GLOBALS',
+                '_SERVER',
+                '_GET',
+                '_POST',
+                '_FILES',
+                '_COOKIE',
+                '_SESSION',
+                '_REQUEST',
+                '_ENV',
+                'argc',
+                'argv',
+                'http_response_header',
             ];
+            $varName = $node->name;
+            if (in_array($varName, $ignored, true)) {
+                return [];
+            }
+
+            if (!preg_match('/^[a-z][a-z0-9]*(_[a-z0-9]+)*$/', $varName)) {
+                return [
+                    RuleErrorBuilder::message(
+                        sprintf('Variable $%s must be snake_case.', $varName)
+                    )->build()
+                ];
+            }
         }
 
+        // Class properties
+        if ($node instanceof Node\Stmt\Property) {
+            $propNames = array_map(
+                static fn(\PhpParser\Node\PropertyItem $prop): string => $prop->name->toString(),
+                $node->props
+            );
+
+            foreach ($propNames as $propName) {
+                if (!preg_match('/^[a-z]+(_[a-z]+)*$/', $propName)) {
+                    $errors[] = RuleErrorBuilder::message(
+                        sprintf('Property $%s must be snake_case.', $propName)
+                    )->build();
+                }
+            }
+            return $errors;
+        }
         return [];
     }
 }
